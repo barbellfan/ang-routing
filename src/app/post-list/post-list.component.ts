26d640703afe6cd598e5e7dbbe5ecc6ac60cc962
Chris Allen <gh.84kjuqg365@gmail.com>
2023-07-03 11:28:20 -0700

    add component for single post. add route to it

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { getLocaleEraNames } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
+  posts = [
+    {
+      id: 1,
+      title: "Title 1",
+      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam blanditiis beatae necessitatibus neque corporis voluptates quisquam nesciunt dolorem, libero, fuga aliquid odit ullam molestiae velit similique, quibusdam illum numquam fugiat."
+    },
+    {
+      id: 2,
+      title: "Title 1",
+      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam blanditiis beatae necessitatibus neque corporis voluptates quisquam nesciunt dolorem, libero, fuga aliquid odit ullam molestiae velit similique, quibusdam illum numquam fugiat."
+    },
+    {
+      id: 3,
+      title: "Title 1",
+      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam blanditiis beatae necessitatibus neque corporis voluptates quisquam nesciunt dolorem, libero, fuga aliquid odit ullam molestiae velit similique, quibusdam illum numquam fugiat."
+    },
+    {
+      id: 4,
+      title: "Title 1",
+      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam blanditiis beatae necessitatibus neque corporis voluptates quisquam nesciunt dolorem, libero, fuga aliquid odit ullam molestiae velit similique, quibusdam illum numquam fugiat."
+    },
+    {
+      id: 5,
+      title: "Title 1",
+      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam blanditiis beatae necessitatibus neque corporis voluptates quisquam nesciunt dolorem, libero, fuga aliquid odit ullam molestiae velit similique, quibusdam illum numquam fugiat."
+    },
+  ];
+
+  constructor() {}
 
 }
