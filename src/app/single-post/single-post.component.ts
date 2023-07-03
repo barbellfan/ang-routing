671f940a1f20a8c3f567ca474462e5de7be9c742
Chris Allen <gh.84kjuqg365@gmail.com>
2023-07-03 13:05:19 -0700

    use activated route to get route param

--- src/app/single-post/single-post.component.ts
+++ src/app/single-post/single-post.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
   styleUrls: ['./single-post.component.css']
 })
-export class SinglePostComponent {
+export class SinglePostComponent implements OnInit {
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(value => {
+      let id = value.get("id");
+      console.log(id);
+
+    });
+  }
 }
